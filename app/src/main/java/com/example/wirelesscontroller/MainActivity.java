7a7f7f73f9b9efde3a52f75a914ff3523ae0b7fe
akshit913 <akshit.9@gmail.com>
2020-05-26 22:02:01 -0400

    Button on click functionality added. Fix socket address

--- app/src/main/java/com/example/wirelesscontroller/MainActivity.java
+++ app/src/main/java/com/example/wirelesscontroller/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.wirelesscontroller;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,11 +10,28 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.provider.SyncStateContract;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Context context;
+    private boolean isConnected=false;
+    private Socket socket;
+    private PrintWriter out;
+    public static int SERVER_PORT = 8000;
+    public static String SERVER_IP = "192.168.56.1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,10 +65,101 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_connect) {
+            ConnectPhoneTask connectPhoneTask = new ConnectPhoneTask();
+            connectPhoneTask.execute(SERVER_IP);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_down:
+                if (isConnected && out!=null) {
+                    out.println("down");//send "play" to server
+                }
+                break;
+            case R.id.btn_up:
+                if (isConnected && out!=null) {
+                    out.println("up"); //send "next" to server
+                }
+                break;
+            case R.id.btn_left:
+                if (isConnected && out!=null) {
+                    out.println("left"); //send "previous" to server
+                }
+                break;
+            case R.id.btn_right:
+                if (isConnected && out!=null) {
+                    out.println("right"); //send "previous" to server
+                }
+                break;
+            case R.id.btn_x:
+                if (isConnected && out!=null) {
+                    out.println("x");//send "play" to server
+                }
+                break;
+            case R.id.btn_triangle:
+                if (isConnected && out!=null) {
+                    out.println("triangle"); //send "next" to server
+                }
+                break;
+            case R.id.btn_circle:
+                if (isConnected && out!=null) {
+                    out.println("circle"); //send "previous" to server
+                }
+                break;
+            case R.id.btn_square:
+                if (isConnected && out!=null) {
+                    out.println("square"); //send "previous" to server
+                }
+                break;
+        }
+    }
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        if(isConnected && out!=null) {
+            try {
+                out.println("exit"); //tell server to exit
+                socket.close(); //close socket
+            } catch (IOException e) {
+                Log.e("remotedroid", "Error in closing socket", e);
+            }
+        }
+    }
+    public class ConnectPhoneTask extends AsyncTask<String,Void,Boolean> {
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            boolean result = true;
+            try {
+                InetAddress serverAddr = InetAddress.getByName(params[0]);
+                socket = new Socket(serverAddr, SERVER_PORT);//Open socket on server IP and port
+            } catch (IOException e) {
+                Log.e("remotedroid", "Error while connecting", e);
+                result = false;
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result)
+        {
+            isConnected = result;
+            Toast.makeText(context,isConnected?"Connected to server!":"Error while connecting", Toast.LENGTH_LONG).show();
+            try {
+                if(isConnected) {
+                    out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket
+                            .getOutputStream())), true); //create output stream to send data to server
+                }
+            }catch (IOException e){
+                Log.e("remotedroid", "Error while creating OutWriter", e);
+                Toast.makeText(context,"Error while connecting",Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
